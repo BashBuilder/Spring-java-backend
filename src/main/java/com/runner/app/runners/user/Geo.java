cc4d341da2cb506a1e1d0dfa345b5c2b58473dea
unknown <iamadelowotan@gmail.com>
2024-09-22 20:22:05 +0100

    rest client initiated

---
+++ src/main/java/com/runner/app/runners/user/Geo.java
@@ -0,0 +1,8 @@
+package com.runner.app.runners.user;
+
+
+public record Geo(
+        String lat,
+        String lng
+) {
+}
---
+++ src/main/java/com/runner/app/runners/user/UserRestClient.java
@@ -0,0 +1,27 @@
+package com.runner.app.runners.user;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+public class UserRestClient {
+
+    private final RestClient restClient;
+
+    public UserRestClient(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+    }
+
+    public List<User> getUsers() {
+        return restClient
+                .get()
+                .uri("/users")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<User>>() {})
+    }
+}
